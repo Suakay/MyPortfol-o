c97917d09ede7a81112526454c82ea8100e0826d
Bengüsu Akay <Bengusu1998@hotmail.com>
2024-10-16 23:55:54 +0300

    Partiallar Oluşturuldu.

---
+++ MyPortfolıo/ViewComponents/SkillComponenetPartial.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolıo.DAL.Context;
+
+namespace MyPortfolıo.ViewComponents
+{
+    public class SkillComponenetPartial: ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public SkillComponenetPartial(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var models=_context.Skills.ToList(); 
+            return View(models);
+        }
+    }
+}
---
+++ MyPortfolıo/ViewComponents/SocialMediaComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolıo.DAL.Context;
+
+namespace MyPortfolıo.ViewComponents
+{
+    public class SocialMediaComponentPartial: ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public SocialMediaComponentPartial(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var socialMediaList = _context.SocialMedias.ToList();
+            return View(socialMediaList.FirstOrDefault());
+
+        }
+    }
+}
